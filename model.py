68ba42895dfde8115302560714ed56b6f1d9b1e7
Divyansh Jha <divyanshj.16@gmail.com>
2017-10-05 00:54:48 +0530

    make training and anlyzing notebooks different. Add many different models in models.py currently only MLP models with different parameters

--- model.py
+++ model.py
@@ -14,4 +14,120 @@ def model1(X,y): 							# simple three layer feedforward network!
 
 	return y_out
 
+def model2(X,y):
+	W1 = tf.get_variable('W1',shape=[2304,5000])
+	b1 = tf.get_variable('b1',shape=[5000])
+	W2 = tf.get_variable('W2',shape=[5000,1000])
+	b2 = tf.get_variable('b2',shape=[1000])
+	W3 = tf.get_variable('W3',shape=[1000,100])
+	b3 = tf.get_variable('b3',shape=[100])
+	W4 = tf.get_variable('W4',shape=[100,100])
+	b4 = tf.get_variable('b4',shape=[100])
+	W5 = tf.get_variable('W5',shape=[100,7])
+	b5 = tf.get_variable('b5',shape=[7])
+
+	out1 = tf.nn.relu(tf.matmul(X,W1) + b1)
+	out2 = tf.nn.relu(tf.matmul(out1,W2) + b2)
+	out3 = tf.nn.relu(tf.matmul(out2,W3) + b3)
+	out4 = tf.nn.relu(tf.matmul(out3,W4) + b4)
+	y_out = tf.matmul(out4,W5) + b5
+
+	return y_out
+
+def model3(X,y):
+	W1 = tf.get_variable('W1',shape=[2304,5000])
+	b1 = tf.get_variable('b1',shape=[5000])
+	W2 = tf.get_variable('W2',shape=[5000,300])
+	b2 = tf.get_variable('b2',shape=[300])
+	W3 = tf.get_variable('W3',shape=[300,100])
+	b3 = tf.get_variable('b3',shape=[100])
+	W4 = tf.get_variable('W4',shape=[100,7])
+	b4 = tf.get_variable('b4',shape=[7])
+
+	out1 = tf.nn.relu(tf.matmul(X,W1) + b1)
+	out2 = tf.nn.relu(tf.matmul(out1,W2) + b2)
+	out3 = tf.nn.relu(tf.matmul(out2,W3) + b3)
+	y_out = tf.matmul(out3,W4) + b4
+
+	return y_out
+
+
+def model4(X,y):
+	W1 = tf.get_variable('W1',shape=[2304,5000])
+	b1 = tf.get_variable('b1',shape=[5000])
+	gamma1 = tf.get_variable('gamma1',shape=[5000])
+	beta1 = tf.get_variable('beta1',shape=[5000])
+
+	W2 = tf.get_variable('W2',shape=[5000,300])
+	b2 = tf.get_variable('b2',shape=[300])
+	gamma2 = tf.get_variable('gamma2',shape=[300])
+	beta2 = tf.get_variable('beta2',shape=[300])
+
+	W3 = tf.get_variable('W3',shape=[300,100])
+	b3 = tf.get_variable('b3',shape=[100])
+	gamma3 = tf.get_variable('gamma3',shape=[100])
+	beta3 = tf.get_variable('beta3',shape=[100])
+
+	W4 = tf.get_variable('W4',shape=[100,7])
+	b4 = tf.get_variable('b4',shape=[7])
+
+	out1 = tf.nn.relu(tf.matmul(X,W1) + b1)
+	mean1,var1 = tf.nn.moments(out1,axes=[0])
+	out1_bn = tf.nn.batch_normalization(out1,mean1,var1,beta1,gamma1,1e-7)
+
+	out2 = tf.nn.relu(tf.matmul(out1_bn,W2) + b2)
+	mean2,var2 = tf.nn.moments(out2,axes=[0])
+	out2_bn = tf.nn.batch_normalization(out2,mean2,var2,beta2,gamma2,1e-7)
+
+	out3 = tf.nn.relu(tf.matmul(out2_bn,W3) + b3)
+	mean3,var3 = tf.nn.moments(out3,axes=[0])
+	out3_bn = tf.nn.batch_normalization(out3,mean3,var3,beta3,gamma3,1e-7)
+
+	y_out = tf.matmul(out3_bn,W4) + b4
+
+	return y_out
+
+def model5(X,y):
+
+	beta0 = tf.get_variable('beta0',shape=[2304])
+	gamma0 = tf.get_variable('gamma0',shape=[2304])
+
+	W1 = tf.get_variable('W1',shape=[2304,5000])
+	b1 = tf.get_variable('b1',shape=[5000])
+	gamma1 = tf.get_variable('gamma1',shape=[5000])
+	beta1 = tf.get_variable('beta1',shape=[5000])
+
+	W2 = tf.get_variable('W2',shape=[5000,300])
+	b2 = tf.get_variable('b2',shape=[300])
+	gamma2 = tf.get_variable('gamma2',shape=[300])
+	beta2 = tf.get_variable('beta2',shape=[300])
+
+	W3 = tf.get_variable('W3',shape=[300,100])
+	b3 = tf.get_variable('b3',shape=[100])
+	gamma3 = tf.get_variable('gamma3',shape=[100])
+	beta3 = tf.get_variable('beta3',shape=[100])
+
+	W4 = tf.get_variable('W4',shape=[100,7])
+	b4 = tf.get_variable('b4',shape=[7])
+
+	mean0,var0 = tf.nn.moments(X,axes=[0])
+	X_bn = tf.nn.batch_normalization(X,mean0,var0,beta0,gamma0,1e-7)
+
+	out1 = tf.nn.relu(tf.matmul(X_bn,W1) + b1)
+	mean1,var1 = tf.nn.moments(out1,axes=[0])
+	out1_bn = tf.nn.batch_normalization(out1,mean1,var1,beta1,gamma1,1e-7)
+
+	out2 = tf.nn.relu(tf.matmul(out1_bn,W2) + b2)
+	mean2,var2 = tf.nn.moments(out2,axes=[0])
+	out2_bn = tf.nn.batch_normalization(out2,mean2,var2,beta2,gamma2,1e-7)
+
+	out3 = tf.nn.relu(tf.matmul(out2_bn,W3) + b3)
+	mean3,var3 = tf.nn.moments(out3,axes=[0])
+	out3_bn = tf.nn.batch_normalization(out3,mean3,var3,beta3,gamma3,1e-7)
+
+	y_out = tf.matmul(out3_bn,W4) + b4
+
+	return y_out
+
+
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def get_subset(df,start=None,end=None,indices=None):
+def get_subset(df,start=None,end=None,indices=None):  # no more use!
 	# print(start is not None,end is not None,indices is not None)
 	assert (start is None or end is None) and indices is not None, ValueError('can only pass indices alone')
 	if start is not None and end is not None:
@@ -16,3 +16,20 @@ def get_subset(df,start=None,end=None,indices=None):
 		return X,y
 	else:
 		return None
+
+def get_emotion_data():
+    X_train = pd.read_csv('data/X_train.csv',header=None,dtype=np.float64).as_matrix()
+    y_train = pd.read_csv('data/y_train.csv',header=None,dtype=np.float64).as_matrix()
+    X_val = pd.read_csv('data/X_val.csv',header=None,dtype=np.float64).as_matrix()
+    y_val = pd.read_csv('data/y_val.csv',header=None,dtype=np.float64).as_matrix()
+    X_test = pd.read_csv('data/X_test.csv',header=None,dtype=np.float64).as_matrix()
+    y_test = pd.read_csv('data/y_test.csv',header=None,dtype=np.float64).as_matrix()
+    
+    mean_image = np.mean(X_train,axis=0)
+    plt.imshow(mean_image.reshape(48,48))
+    
+    X_train -= mean_image
+    X_val -= mean_image
+    X_test -= mean_image
+    
+    return X_train, y_train, X_val, y_val, X_test, y_test
